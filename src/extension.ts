37d81779fc60acd30cf0e618c1a0c9c54293d1a9
Chetan <chetanrjj2002@gmail.com>
2025-01-29 16:47:17 +0530

    created a skeleton panel for chat

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,38 @@ import * as vscode from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "deepseek-for-vscode" is now active!');
+	console.log('"deepseek-for-vscode" is now active!');
 
-	const disposable = vscode.commands.registerCommand('deepseek-for-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from deepseek for vscode!');
+	const disposable = vscode.commands.registerCommand('deepseek-for-vscode.chat', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'deepseek_chat',
+			'Deepseek Chat',
+			vscode.ViewColumn.One,
+			{enableScripts:true}
+		)
+
+		panel.webview.html = getWebviewContent();
+		
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+
+function getWebviewContent():string{
+	return /*html*/`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<style>
+			
+			</style>
+		</head>
+		<body>
+			<h1>Deepseek for vscode</h1>
+		</body>
+		</html>
+	`
+}
 export function deactivate() {}
