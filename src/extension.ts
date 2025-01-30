f242a3be824883d3ffc9dbbf04a6dd860719a1cb
Chetan <chetanrjj2002@gmail.com>
2025-01-30 13:08:02 +0530

    added functionality to chat with the local ollama model

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
+import Ollama from 'ollama';
 import * as vscode from 'vscode';
-
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"deepseek-for-vscode" is now active!');
@@ -13,6 +13,26 @@ export function activate(context: vscode.ExtensionContext) {
 		)
 
 		panel.webview.html = getWebviewContent();
+
+		panel.webview.onDidReceiveMessage(async (message:any) => {
+			if (message.command == "chat"){
+				const userPrompt = message.text;
+				let responseText = ''
+				try{
+					const streamResponse = await Ollama.chat({
+						model:"deepseek-r1:1.5b",
+						messages:[{role:'user', content:userPrompt}],
+						stream:true
+					})
+					for await (const part of streamResponse){// loop asynchronously
+						responseText += part.message.content;
+						panel.webview.postMessage({command:"chatResponse", text:responseText})
+					}
+				} catch(err){
+					panel.webview.postMessage({command:'chatResponse', text:`Error: ${String(err)}`})
+				}
+			}
+		});
 		
 	});
 
@@ -27,11 +47,29 @@ function getWebviewContent():string{
 		<head>
 			<meta charset="UTF-8">
 			<style>
-			
+		
 			</style>
 		</head>
 		<body>
-			<h1>Deepseek for vscode</h1>
+			<h2>Deepseek for vscode</h2>
+			<textarea id="prompt" rows="3" placeholder="Ask something..."></textarea>
+			<button id="askButton">Ask</button>
+			<div id="response"></div>
+			<script>
+				const vscode = acquireVsCodeApi();
+				console.log('live')
+				document.getElementById("askButton").addEventListener("click", () => {
+					const prompt = document.getElementById("prompt").value;
+					vscode.postMessage({command:"chat", text:prompt});
+				});
+				
+				window.addEventListener("message", event => {
+					const {command, text} = event.data;
+					if (command == "chatResponse"){
+						document.getElementById("response").innerText = text;
+					}
+				})
+			</script>
 		</body>
 		</html>
 	`
