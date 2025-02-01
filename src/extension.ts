cce00ed330fda79ba80c7c30ee20f5f2b90ef1fd
Chetan <chetanrjj2002@gmail.com>
2025-02-01 12:07:29 +0530

    added styles for dropdown menu and fixed position:

--- src/extension.ts
+++ src/extension.ts
@@ -42,6 +42,7 @@ export function activate(context: vscode.ExtensionContext) {
 				//user globalState if you want the data to persist after closing vs code
 				let messages:Message[] = context.workspaceState.get("chatHistory") ?? []; // data stored only for current session
 				messages.push(userMessage);
+				console.log(messages)
 				let responseText = "";
 				try{
 					const streamResponse = await Ollama.chat({
@@ -75,69 +76,142 @@ export function activate(context: vscode.ExtensionContext) {
 
 function getWebviewContent():string{
 	return /*html*/`
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<style>
-				@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
-				*{
-					font-family: "Rajdhani", serif;
-				}
-				body{
-					margin: 1rem;
-				}
-				#prompt{
-					width: 100%;
-					box-sizing: border-box;
-				}
-				.response{
-					border: 1px solid #ccc;
-					margin-top: 1rem;
-					padding: 0.5rem;
-				}
+	<!DOCTYPE html>
+	<html lang="en">
 
-			</style>
-		</head>
-		<body>
-			<h2>Deepseek for vscode</h2>
-			<textarea id="prompt" rows="3" placeholder="Ask something..."></textarea>
-			<button id="askButton">Ask</button>
-			<select id="modelsDropdown"></select>
-			<div id="chatsContainer"></div>
-			<script>
-				const vscode = acquireVsCodeApi();
-				const askBtn = document.getElementById("askButton");
-				const dropdown = document.getElementById("modelsDropdown");
-				const container = document.getElementById("chatsContainer");
-				let currentResponseDiv = null; // track active response div
+	<head>
+		<meta charset="UTF-8">
+		<style>
+			@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
 
-				askBtn.addEventListener("click", () => {
-					const prompt = document.getElementById("prompt").value;
-					askBtn.disabled = true;
-					vscode.postMessage({command:"chat", text:prompt, selectedModel: dropdown.value});
-				});
-				
-				window.addEventListener("message", event => {
-					const {command, text, models} = event.data;
-					if (command == "chatResponse"){
-						if(!currentResponseDiv){
-							currentResponseDiv = document.createElement("div");
-							currentResponseDiv.classList.add("response")
-							container.appendChild(currentResponseDiv);
-						}
-					
-						currentResponseDiv.innerText = text;
-					} else if(command == "responseEnd"){
-						askBtn.disabled = false;
-						currentResponseDiv = null // reset for next response
-					} else if(command == "modelsList"){
-						dropdown.innerHTML = models.map(model => '<option value="' + model + '">' + model + '</option>').join('');
+			* {
+				color: whitesmoke;
+				font-family: "Rajdhani", serif;
+				box-sizing: border-box;
+			}
+
+			body {
+				margin: 1rem;
+				background-color: #1a1a1a;
+			}
+
+			#prompt {
+				background-color: transparent;
+				width: 100%;
+				outline:none;
+				resize: none;
+				border: none;
+			}
+			#promptContainer{
+				position:fixed;
+				bottom: 0;
+				width: 100%;
+			}
+			#inputBox{
+				background-color: #2d2d2d;
+				width: 50%;
+				text-align: center;
+				margin: 0 auto;
+				border-radius: 0.4rem;
+				padding: 0.6rem 0.6rem 0 0.6rem;
+			}
+			#askButton{
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				border-radius: 50%;
+				border: none;
+				height: 1.7rem;
+				width: 1.7rem;
+				cursor: pointer;
+				margin-left: 1rem;
+			}
+			#modelsDropdown{
+				width: 8rem;
+				height: 1.5rem;
+				background-color: #2d2d2d;
+				border-radius: 0.3rem;
+				border: none;
+				text-align: center;
+			}
+			#modelsDropdown:hover{
+				background-color: #1a1a1a;
+				cursor: pointer;
+			}
+			#optionsContainer{
+				display: flex;
+				align-items: center;
+				justify-content: start;
+				padding-left: 1rem;
+			}
+
+			.response {
+				border: 1px solid #ccc;
+				margin-top: 1rem;
+				padding: 0.5rem;
+			}
+		</style>
+	</head>
+
+	<body>
+		<h2>Deepseek for vscode</h2>
+		<div id="promptContainer">
+			<div id="inputBox">
+				<div>
+					<textarea id="prompt" rows="3" placeholder="Ask something..."></textarea>
+					<div id="optionsContainer">
+						<select id="modelsDropdown">
+							<option value="" disabled selected>Select a model</option>
+							<option value="">Select a model</option>
+							<option value="">Select a model</option>
+							<option value="">Select a model</option>
+						</select>
+						<button id="askButton"">
+							<svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M7 16c-.595 0-1.077-.462-1.077-1.032V1.032C5.923.462 6.405 0 7 0s1.077.462 1.077 1.032v13.936C8.077 15.538 7.595 16 7 16z" fill="#2a2a2a"></path>
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M.315 7.44a1.002 1.002 0 0 1 0-1.46L6.238.302a1.11 1.11 0 0 1 1.523 0c.421.403.421 1.057 0 1.46L1.838 7.44a1.11 1.11 0 0 1-1.523 0z" fill="#2a2a2a"></path>
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M13.685 7.44a1.11 1.11 0 0 1-1.523 0L6.238 1.762a1.002 1.002 0 0 1 0-1.46 1.11 1.11 0 0 1 1.523 0l5.924 5.678c.42.403.42 1.056 0 1.46z" fill="#2a2a2a"></path>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="chatsContainer"></div>
+		<script>
+			const vscode = acquireVsCodeApi();
+			const askBtn = document.getElementById("askButton");
+			const dropdown = document.getElementById("modelsDropdown");
+			const container = document.getElementById("chatsContainer");
+			let currentResponseDiv = null; // track active response div
+
+			askBtn.addEventListener("click", () => {
+				const prompt = document.getElementById("prompt").value;
+				askBtn.disabled = true;
+				vscode.postMessage({ command: "chat", text: prompt, selectedModel: dropdown.value });
+			});
+
+			window.addEventListener("message", event => {
+				const { command, text, models } = event.data;
+				if (command == "chatResponse") {
+					if (!currentResponseDiv) {
+						currentResponseDiv = document.createElement("div");
+						currentResponseDiv.classList.add("response")
+						container.appendChild(currentResponseDiv);
 					}
-				})
-			</script>
-		</body>
-		</html>
+
+					currentResponseDiv.innerText = text;
+				} else if (command == "responseEnd") {
+					askBtn.disabled = false;
+					currentResponseDiv = null // reset for next response
+				} else if (command == "modelsList") {
+					dropdown.innerHTML = '<option value="" disabled selected>Select a model</option>' + models.map(model => '<option value="' + model + '">' + model + '</option>').join('');
+				}
+			})
+		</script>
+	</body>
+
+	</html>
 	`
 }
 export function deactivate() {}
